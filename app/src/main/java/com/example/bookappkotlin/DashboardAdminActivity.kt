597037430d1c66ea0d63c507a2c195bbebb556a1
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-04-19 13:28:35 +0700

    [fix] SplashActivity transfer to Dashboard

--- app/src/main/java/com/example/bookappkotlin/DashboardAdminActivity.kt
+++ app/src/main/java/com/example/bookappkotlin/DashboardAdminActivity.kt
@@ -1,14 +1,51 @@
 package com.example.bookappkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bookappkotlin.databinding.ActivityDashboardAdminBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class DashboardAdminActivity : AppCompatActivity() {
 
+    // view binding
+    private lateinit var binding: ActivityDashboardAdminBinding
 
+    // firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard_admin)
+        binding = ActivityDashboardAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // init firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        // handle click, logout
+        binding.logoutBtn.setOnClickListener {
+            firebaseAuth.signOut()
+            checkUser()
+        }
+    }
+
+    private fun checkUser() {
+        // get current user
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == null) {
+            // not logged in, goto main screen
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+        else {
+            // logged in, get and show user info
+            val email = firebaseUser.email
+
+            // set to textview of toolbar
+            binding.subTitleTv.text = email
+
+
+        }
     }
 }
--- app/src/main/java/com/example/bookappkotlin/SplashActivity.kt
+++ app/src/main/java/com/example/bookappkotlin/SplashActivity.kt
@@ -4,15 +4,67 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.example.bookappkotlin.databinding.ActivitySplashBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class SplashActivity : AppCompatActivity() {
+
+    // view binding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         Handler().postDelayed(Runnable {
+            checkUser()
+        }, 1000) // means 1 seconds
+    }
+
+    private fun checkUser() {
+        // get current user, if logged in or not
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser == null) {
+            // user not logged in, goto main screen
             startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 2000) // means 2 seconds
+        }
+        else {
+            // user logged in, check user type, save as done in login screen
+            val ref = FirebaseDatabase.getInstance().getReference("Users")
+            ref.child(firebaseUser.uid)
+                .addListenerForSingleValueEvent(object: ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        // get user type e.g. user or admin
+                        val userType = snapshot.child("userType").value
+                        if (userType == "user") {
+                            // open User Dashboard
+                            startActivity(Intent(this@SplashActivity, DashboardUserActivity::class.java))
+                            finish()
+                        }
+                        else if (userType == "admin") {
+                            // open Admin Dashboard
+                            startActivity(Intent(this@SplashActivity, DashboardAdminActivity::class.java))
+                            finish()
+                        }
+                        else {
+
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+
+                    }
+                })
+        }
     }
 }
+
+// Keep user logged in
+// 1. Check if user logged in
+// 2. Check type of user
