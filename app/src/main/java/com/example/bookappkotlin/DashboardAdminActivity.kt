7b02ce185db90d2746d3b86e9aa12c7aee68ddec
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-04-19 11:02:22 +0700

    [add] validateUser and loginUser

--- app/src/main/java/com/example/bookappkotlin/DashboardAdminActivity.kt
+++ app/src/main/java/com/example/bookappkotlin/DashboardAdminActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class DashboardAdminActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard_admin)
--- app/src/main/java/com/example/bookappkotlin/LoginActivity.kt
+++ app/src/main/java/com/example/bookappkotlin/LoginActivity.kt
@@ -1,11 +1,130 @@
 package com.example.bookappkotlin
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.example.bookappkotlin.databinding.ActivityLoginBinding
+import com.example.bookappkotlin.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
+
+    // view binding
+    private lateinit var binding:ActivityLoginBinding
+
+    // firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    // progress dialog
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // init progress dialog, will show while login user
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        // handle click, not have account, goto register screen
+        binding.noAccountTv.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        // handle click, begin login
+        binding.loginBtn.setOnClickListener {
+            // 1. Input Data
+            // 2. Validate Data
+            // 3. Login - Firebase Auth
+            // 4. Check user type - Firebase Auth
+            //  If User -> User Dashboard
+            //  If Admin -> Admin Dashboard
+        }
+    }
+
+    private var email = ""
+    private var password = ""
+
+    private fun validateData() {
+        // 1. Input Data
+        email = binding.emailEt.text.toString().trim()
+        password = binding.passwordEt.text.toString().trim()
+
+        // 2. Validate data
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Invalid email format...", Toast.LENGTH_SHORT).show()
+        }
+        else if (password.isEmpty()) {
+            Toast.makeText(this, "Enter password...", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+        // 3. Login - Firebase Auth
+
+        // show progress
+        progressDialog.setMessage("Logging in...")
+        progressDialog.show()
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                // login success
+                checkUser()
+            }
+            .addOnFailureListener {e->
+                // failed login
+                progressDialog.dismiss()
+                Toast.makeText(this, "Login failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun checkUser() {
+        progressDialog.setMessage("Checking User...")
+
+        val firebaseUser = firebaseAuth.currentUser!!
+
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(firebaseUser.uid)
+            .addListenerForSingleValueEvent(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    progressDialog.dismiss()
+
+                    // get user type e.g. user or admin
+                    val userType = snapshot.child("userType").value
+                    if (userType == "user") {
+                        // open User Dashboard
+                        startActivity(Intent(this@LoginActivity, DashboardUserActivity::class.java))
+                        finish()
+                    }
+                    else if (userType == "admin") {
+                        // open Admin Dashboard
+                        startActivity(Intent(this@LoginActivity, DashboardAdminActivity::class.java))
+                        finish()
+                    }
+                    else {
+
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+            })
     }
 }
