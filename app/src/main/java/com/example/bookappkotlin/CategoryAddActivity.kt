5f5dd63b1e6ba618c980542c0c9ce0ea334ce135
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-04-21 11:42:34 +0700

    [add] Add Category into Firebase DB

--- app/src/main/java/com/example/bookappkotlin/CategoryAddActivity.kt
+++ app/src/main/java/com/example/bookappkotlin/CategoryAddActivity.kt
@@ -1,11 +1,93 @@
 package com.example.bookappkotlin
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AlertDialog
+import com.example.bookappkotlin.databinding.ActivityCategoryAddBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class CategoryAddActivity : AppCompatActivity() {
+
+    // view binding
+    private lateinit var binding:ActivityCategoryAddBinding
+
+    // firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    // progress dialog
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_category_add)
+        binding = ActivityCategoryAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // configure progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        // handle click, go back
+        binding.backBtn.setOnClickListener {
+            onBackPressed()
+        }
+
+        // handle click, begin upload category
+        binding.submitBtn.setOnClickListener {
+            validateData()
+        }
+    }
+
+    private var category = ""
+
+    private fun validateData() {
+        // get data
+        category = binding.categoryEt.text.toString().trim()
+
+        // validate data
+        if (category.isEmpty()) {
+            Toast.makeText(this, "Enter Category...", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            addCategoryFirebase()
+        }
+    }
+
+    private fun addCategoryFirebase() {
+        // show progress
+        progressDialog.show()
+
+        // get timestamp
+        val timestamp = System.currentTimeMillis()
+
+        // setup data to add in firebase db
+        val hashMap = HashMap<String, Any>()
+        hashMap["id"] = "$timestamp"
+        hashMap["category"] = category
+        hashMap["timestamp"] = timestamp
+        hashMap["uid"] = "${firebaseAuth.uid}"
+
+        // add to firebase db: Database Root > Categories > categoryId > category info
+        val ref = FirebaseDatabase.getInstance().getReference("Categories")
+        ref.child("$timestamp")
+            .setValue(hashMap)
+            .addOnSuccessListener {
+                // added successfully
+                progressDialog.dismiss()
+                binding.categoryEt.setText("")
+                Toast.makeText(this, "Added Successfully...", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {e ->
+                // failed to add
+                progressDialog.dismiss()
+                Toast.makeText(this, "Failed to add due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
